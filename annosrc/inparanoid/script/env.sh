e8c5d57aa411c8143ecc3b6f99be2e8eee7bfaf6
Valerie Obenchain <vobencha@gmail.com>
2019-04-03 20:33:06 +0000

    Modifications to ucsc/script/env.sh and inparanoid/script/env.sh

--- annosrc/inparanoid/script/env.sh
+++ annosrc/inparanoid/script/env.sh
@@ -11,9 +11,9 @@ export IPSOURCEURL="http://inparanoid.sbc.su.se/download/current/sqltables/"
 # export ENSOURCENAME="Ensembl"
 # export ENSOURCEURL="ftp://ftp.ensembl.org/pub/current_fasta"
 
-export FBSOURCEDATE=2017-Feb01
+export FBSOURCEDATE=2019-Feb13
 export FBSOURCENAME="Flybase"
 export FBSOURCEURL="ftp://ftp.flybase.net/releases/current/precomputed_files/genes/"
 ## Check and rename each time 
-FILE="fbgn_fbtr_fbpp_fb_2017_01.tsv.gz"
-UZFILE="fbgn_fbtr_fbpp_fb_2017_01.tsv"
+FILE="fbgn_fbtr_fbpp_fb_2019_01.tsv.gz"
+UZFILE="fbgn_fbtr_fbpp_fb_2019_01.tsv"
