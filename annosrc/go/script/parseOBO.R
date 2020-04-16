52fee0d41fbfb0a296ea2a290c8db94be60f68c1
Daniel Van Twisk <daniel.vantwisk@roswellpark.org>
2020-04-16 12:12:54 -0400

    parseOBO.R script completed

--- annosrc/go/script/parseOBO.R
+++ annosrc/go/script/parseOBO.R
@@ -23,9 +23,7 @@ names <- c(names, synonym_scope)    ## Add synonym_scope
 terms <- data.frame(id = seq_along(names),
                     name = names)
 
-term_id_names <- terms
-
-temp_df <- kv[kv$key == 'namespace',][-2]
+temp_df <- kv[kv$key == 'namespace',][-2]  ## extract term relationships from kv
 names(temp_df) <- c('name', 'term_type')
 term <- dplyr::left_join(terms, temp_df)
 
@@ -43,6 +41,8 @@ term[term$name == 'is_a', 'term_type'] <- 'relationship'
 term[term$name %in% synonym_type, 'term_type'] <- 'synonym_type'
 term[term$name %in% synonym_scope, 'term_type'] <- 'synonym_scope'
 
+write.table(term, file = term_f, quote=F, col.names=F, row.names=F)
+
 
 ## Create term2term table
 
@@ -55,7 +55,7 @@ colnames(relations) <- c('term1_id', 'relationship_type_id', 'term2_id')
 
 relations <- rbind(is_a, relations)
 
-relations[[1]] <- match(relations[[1]], names)
+relations[[1]] <- match(relations[[1]], names)      # Change GO names into the ids we assigned form the term table
 relations[[2]] <- match(relations[[2]], names)
 relations[[3]] <- match(relations[[3]], names)
 
@@ -64,6 +64,7 @@ term2term['id'] <- seq_along(nrow(term2term))
 term2term['complete'] <- 0
 term2term <- term2term[c(4, 2, 1, 3, 5)]
 
+write.table(term2term, file = term2term_f, quote=F, col.names=F, row.names=F)
 
 
 ## term_synonym.txt
@@ -74,7 +75,7 @@ alt_id['acc_synonym'] <- alt_id['term_synonym']
 alt_id['synonym_category_id'] <- "\\N"
 
 synonym <- kv[kv$key == 'synonym',]
-by_synonym <- lapply(synonym_scope, function(s) {
+by_synonym <- lapply(synonym_scope, function(s) {               # This extracts synonyms from our kv data.frame and parses them to adhere to the term_synonym table
     val <- synonym[grep(toupper(s), synonym$value),][-2]
     val$value <- stringi::stri_extract_first_regex(val$value, '(?<=").*?(?=")')
     val['synonym_type_id'] <- s
@@ -93,6 +94,7 @@ term_synonym$synonym_type_id <- match(term_synonym$synonym_type_id, names)
 
 term_synonym <- term_synonym[c(1, 3, 4, 2, 5)]
     
+write.table(term_synonym, file = term_synonym_f, quote=F, col.names=F, row.names=F)
 
 
 ## term_definition.txt
@@ -113,11 +115,38 @@ term_definition['reference'] <- '\\N'
 
 term_definition <- term_definition[c(1, 2, 4, 3, 5)]
 
+
+write.table(term_defnition, file = term_definition_f, quote=F, col.names=F, row.names=F)
+
+
 ## graph_path.txt
+
+## NOTE: This section generalizes the file to only show generate edges in the transitive closure
+## We assume that the shortest distances are not going to be needed, so those are set to one.
 
 
 V <- as.character(seq_along(names))
 E <- with(term2term, split(term1_id, term2_id))
-gg <- graphNEL(V, E, "directed")
+gg <- graph::graphNEL(V, E, "directed")
 
-tc <- RBGL::transitive.closure()
+tc <- RBGL::transitive.closure(gg)
+
+edges <- graph::edges(tc)
+edges <- lapply(names(edges), function(x){   # This essentially performs an unsplit operation to get our adjacency data.frame back
+    e1 <- edges[[x]]
+    e2 <- rep(x, length(e1))
+    data.frame(V2 = e1, V3 = e2)
+})
+
+graph_path <- do.call(rbind, edges)
+graph_path['V1'] <- seq_len(nrow(graph_path))
+graph_path <- graph_path[, c(3, 1, 2)]
+
+ones <- rep(1, nrow(graph_path))
+graph_path['V4'] <- ones
+graph_path['V5'] <- ones
+graph_path['V6'] <- ones
+
+
+write.table(graph_path, file = graph_path_f, quote=F, col.names=F, row.names=F)
+
