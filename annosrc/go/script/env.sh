008d8c648cd0bf8d67369f68c0bd4760c17f7bbc
Vince Carey <stvj@channing.harvard.edu>
2026-01-22 15:18:06 +0000

    needed improvement to env.sh for GO OBO

--- annosrc/go/script/env.sh
+++ annosrc/go/script/env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-export GOSOURCEDATE=2024-09-08
+export GOSOURCEDATE=2025-10-10
 export GOSOURCENAME="Gene Ontology"
-export GOSOURCEURL="http://current.geneontology.org/ontology/go-basic.obo"
+export GOSOURCEURL="https://current.geneontology.org/ontology/go-basic.obo"
 #export GOSOURCEURL="ftp://ftp.geneontology.org/pub/go/godatabase/archive/latest-lite/"
 #export FILE="go_weekly-termdb-tables.tar.gz"
