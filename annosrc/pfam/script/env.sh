8de640f8b6de555d834056447067c2b57a5db554
Vince Carey <stvj@channing.harvard.edu>
2026-01-22 16:34:01 +0000

    the src_download logic actually changes contents of env.sh

--- annosrc/pfam/script/env.sh
+++ annosrc/pfam/script/env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-export PFAMSOURCEDATE=2024-May28
+export PFAMSOURCEDATE=2025-Jul02
 export PFAMSOURCENAME="PFAM IDs"
 export PFAMSOURCEURL="ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release"
--- annosrc/yeast/script/env.sh
+++ annosrc/yeast/script/env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-export YGSOURCEDATE=2024-Sep17
+export YGSOURCEDATE=2026-Jan20
 export YGSOURCENAME="Yeast Genome"
 export YGSOURCEURL="http://sgd-archive.yeastgenome.org"
